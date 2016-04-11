d4e838ebd515bd769270de4b170a06612fee24ba
Merritt Tidwell <merritt.tidwell@gmail.com>
2016-04-11 16:44:07 -0700

    fixed file structure and updated file for uploading to pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name='tealium',
+  packages=['tealium'],
+  version='1.0',
+  description='Tealium Libary',
+  author='Tealium Integrations Team',
+  author_email='integrations.tealium@tealium.com',
+  url='https://github.com/Tealium/tealium-python',
+  download_url='https://github.com/Tealium/tealium-python/tarball/0.1',
+  keywords=['tealium']
+)
--- tealium/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='tealium',
-      version='1.0',
-      author='Tealium Integrations Team',
-      author_email='integrations.device@tealium.com',
-      packages=['tealium'],
-      zip_safe=False)
